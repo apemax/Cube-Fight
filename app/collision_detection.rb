574f43293869be79129f754eb08ca7ff24f97187
Peter Wright <apemax18@gmail.com>
2026-02-22 19:35:57 +0000

    Add collision detection for the cubes.

--- app/collision_detection.rb
+++ app/collision_detection.rb
@@ -8,7 +8,6 @@ def collision_detection args
   args.state.cpu_one_fist_right[:x] += args.state.cpu_one_fist_right[:dx]
   args.state.cpu_one_fist_left[:x] += args.state.cpu_one_fist_left[:dx]
 
-=begin
   if args.state.player_one.intersect_rect? args.state.cpu_one
     if args.state.player_one.dx > 0
       args.state.player_one.x = args.state.cpu_one.x - args.state.player_one.w
@@ -16,8 +15,8 @@ def collision_detection args
       args.state.player_one_fist_right.x = args.state.cpu_one.x - args.state.player_one_fist_right.w
     elsif args.state.player_one.dx < 0
       args.state.player_one.x = args.state.cpu_one.x + args.state.cpu_one.w
-      args.state.player_one_fist_left.x = args.state.cpu_one.x + args.state.player_one_fist_left.w
-      args.state.player_one_fist_right.x = args.state.cpu_one.x + args.state.player_one_fist_right.w
+      args.state.player_one_fist_left.x = args.state.cpu_one.x + args.state.player_one_fist_left.w + args.state.cpu_one.w
+      args.state.player_one_fist_right.x = args.state.cpu_one.x + args.state.player_one_fist_right.w + args.state.cpu_one.w
     end
     args.state.player_one.dx = 0
     args.state.player_one_fist_left.dx = 0
@@ -27,18 +26,20 @@ def collision_detection args
   if args.state.cpu_one.intersect_rect? args.state.player_one
     if args.state.cpu_one.dx > 0
       args.state.cpu_one.x = args.state.player_one.x - args.state.cpu_one.w
-      args.state.cpu_one_fist_left.x = args.state.player_one.x - args.state.cpu_one_fist_left.w
-      args.state.cpu_one_fist_right.x = args.state.player_one.x - args.state.cpu_one_fist_right.w
+      args.state.cpu_one_fist_left.x = args.state.player_one.x - args.state.cpu_one.w
+      args.state.cpu_one_fist_right.x = args.state.player_one.x - args.state.cpu_one.w
+      args.state.cpu_one_attack_zone.x = args.state.cpu_one.x - 64
     elsif args.state.cpu_one.dx < 0
       args.state.cpu_one.x = args.state.player_one.x + args.state.cpu_one.w
-      args.state.cpu_one_fist_left.x = args.state.player_one.x + args.state.cpu_one_fist_left.w
-      args.state.cpu_one_fist_right.x = args.state.player_one.x + args.state.cpu_one_fist_right.w
+      args.state.cpu_one_fist_left.x = args.state.player_one.x + args.state.player_one.w
+      args.state.cpu_one_fist_right.x = args.state.player_one.x + args.state.player_one.w
+      args.state.cpu_one_attack_zone.x = args.state.cpu_one.x - 64
     end
     args.state.cpu_one.dx = 0
     args.state.cpu_one_fist_left.dx = 0
     args.state.cpu_one_fist_right.dx = 0
+    args.state.cpu_one_attack_zone.dx = 0
   end
-=end
 
   args.state.player_one[:y] += args.state.player_one[:dy]
   args.state.player_one_fist_right[:y] += args.state.player_one_fist_right[:dy]
@@ -49,15 +50,15 @@ def collision_detection args
   args.state.cpu_one_fist_right[:y] += args.state.cpu_one_fist_right[:dy]
   args.state.cpu_one_fist_left[:y] += args.state.cpu_one_fist_left[:dy]
 
-=begin
   if args.state.player_one.intersect_rect? args.state.cpu_one
     if args.state.player_one.dy > 0
       args.state.player_one.y = args.state.cpu_one.y - args.state.player_one.h
-      args.state.player_one_fist_left.y = args.state.cpu_one.y - args.state.player_one_fist_left.h
-      args.state.player_one_fist_right.y = args.state.cpu_one.y - args.state.player_one_fist_right.h
+      args.state.player_one_fist_left.y = args.state.cpu_one.y - args.state.player_one_fist_left.h - 16
+      args.state.player_one_fist_right.y = args.state.cpu_one.y - args.state.player_one_fist_right.h - 16 - 32
     elsif args.state.player_one.dy < 0
-      args.state.player_one_fist_left.y = args.state.cpu_one.y + args.state.player_one_fist_left.h
-      args.state.player_one_fist_right.y = args.state.cpu_one.y + args.state.player_one_fist_right.h
+      args.state.player_one.y = args.state.cpu_one.y + args.state.player_one.h
+      args.state.player_one_fist_left.y = args.state.cpu_one.y + args.state.player_one_fist_left.h + 48
+      args.state.player_one_fist_right.y = args.state.cpu_one.y + args.state.player_one_fist_right.h + 48 + 32
     end
     args.state.player_one.dy = 0
     args.state.player_one_fist_left.dy = 0
@@ -67,15 +68,18 @@ def collision_detection args
   if args.state.cpu_one.intersect_rect? args.state.player_one
     if args.state.cpu_one.dy > 0
       args.state.cpu_one.y = args.state.player_one.y - args.state.cpu_one.h
-      args.state.cpu_one_fist_left.y = args.state.player_one.y - args.state.cpu_one_fist_left.h
-      args.state.cpu_one_fist_right.y = args.state.player_one.y - args.state.cpu_one_fist_right.h
+      args.state.cpu_one_fist_left.y = args.state.player_one.y - args.state.cpu_one_fist_left.h - 16
+      args.state.cpu_one_fist_right.y = args.state.player_one.y - args.state.cpu_one_fist_right.h - 16 - 32
+      args.state.cpu_one_attack_zone.y = args.state.cpu_one.y - 64
     elsif args.state.cpu_one.dy < 0
-      args.state.cpu_one_fist_left.y = args.state.player_one.y + args.state.cpu_one_fist_left.h
-      args.state.cpu_one_fist_right.y = args.state.player_one.y + args.state.cpu_one_fist_right.h
+      args.state.cpu_one.y = args.state.player_one.y + args.state.cpu_one.h
+      args.state.cpu_one_fist_left.y = args.state.player_one.y + args.state.cpu_one_fist_left.h + 48
+      args.state.cpu_one_fist_right.y = args.state.player_one.y + args.state.cpu_one_fist_right.h + 48 + 32
+      args.state.cpu_one_attack_zone.y = args.state.cpu_one.y - 64
     end
     args.state.cpu_one.dy = 0
     args.state.cpu_one_fist_left.dy = 0
     args.state.cpu_one_fist_right.dy = 0
+    args.state.cpu_one_attack_zone.dy = 0
   end
-=end
 end

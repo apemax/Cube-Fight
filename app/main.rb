7a70c3f580786a5d1a8c82a0908ccd1a56db4420
Peter Wright <apemax18@gmail.com>
2026-07-07 19:32:23 +0100

    Add options menu, Add disable CPU attack warning option.

--- app/main.rb
+++ app/main.rb
@@ -5,6 +5,8 @@ require 'app/collision_detection.rb'
 
 def boot args
   args.state = {}
+  args.state.cpu_attack_warning ||= true
+  args.state.menu_option_click_cooldown ||= 0
 end
 
 def tick args
@@ -17,6 +19,8 @@ def tick args
     tick_menu_scene args
   when :how_to_play_scene
     tick_how_to_play_scene args
+  when :options_scene
+    tick_options_scene args
   when :game_scene
     tick_game_scene args
   when :game_over_scene
@@ -38,56 +42,71 @@ end
 def tick_menu_scene args
   args.state.menu_option_player_vs_cpu ||= {x: 180, y: 416, w: 384, h: 64}
   args.state.menu_option_how_to_play ||= {x: 180, y: 316, w: 384, h: 64}
-  args.state.menu_option_exit ||= {x: 180, y: 216, w: 384, h: 64}
+  args.state.menu_option_options ||= {x: 180, y: 216, w: 384, h: 64}
+  args.state.menu_option_exit ||= {x: 180, y: 116, w: 384, h: 64}
   args.state.menu_option_outline ||= [x: 1300, y: 800, w: 256, h: 64, path: 'sprites/menu-option-outline.png']
-  args.state.menu_option ||= 1
+  args.state.menu_option_main ||= 1
   args.state.menu_option_cooldown ||= 0
 
-  if args.inputs.up and args.state.menu_option_cooldown <= 0 and args.state.menu_option >= 2
+  if args.inputs.up and args.state.menu_option_cooldown <= 0 and args.state.menu_option_main >= 2
     args.state.menu_option_cooldown += 10
-    args.state.menu_option -= 1
+    args.state.menu_option_main -= 1
   end
-  if args.inputs.down and args.state.menu_option_cooldown <= 0 and args.state.menu_option <= 2
+  if args.inputs.down and args.state.menu_option_cooldown <= 0 and args.state.menu_option_main <= 3
     args.state.menu_option_cooldown += 10
-    args.state.menu_option += 1
+    args.state.menu_option_main += 1
   end
 
   if args.state.menu_option_cooldown > 0
     args.state.menu_option_cooldown -= 1
   end
+  if args.state.menu_option_click_cooldown > 0
+    args.state.menu_option_click_cooldown -= 1
+  end
 
   if args.inputs.mouse.intersect_rect?(args.state.menu_option_player_vs_cpu)
-    args.state.menu_option = 1
+    args.state.menu_option_main = 1
   end
   if args.inputs.mouse.intersect_rect?(args.state.menu_option_how_to_play)
-    args.state.menu_option = 2
+    args.state.menu_option_main = 2
+  end
+  if args.inputs.mouse.intersect_rect?(args.state.menu_option_options)
+    args.state.menu_option_main = 3
   end
   if args.inputs.mouse.intersect_rect?(args.state.menu_option_exit)
-    args.state.menu_option = 3
+    args.state.menu_option_main = 4
   end
 
   args.outputs.background_color = [255, 255, 255]
   args.outputs.labels << {x: 180, y: 620, text: "20 Second Cube Fight", size_enum: 40, a: 255, r: 0, g: 0, b: 0}
   args.outputs.labels << {x: 180, y: 480, text: "Player VS CPU", size_enum: 20, a: 255, r: 0, g: 0, b: 0}
   args.outputs.labels << {x: 180, y: 380, text: "How to Play", size_enum: 20, a: 255, r: 0, g: 0, b: 0}
-  args.outputs.labels << {x: 180, y: 280, text: "Exit", size_enum: 20, a: 255, r: 0, g: 0, b: 0}
+  args.outputs.labels << {x: 180, y: 280, text: "Options", size_enum: 20, a: 255, r: 0, g: 0, b: 0}
+  args.outputs.labels << {x: 180, y: 180, text: "Exit", size_enum: 20, a: 255, r: 0, g: 0, b: 0}
   args.outputs.primitives << args.state.menu_option_outline
 
-  if args.state.menu_option == 1
+  if args.state.menu_option_main == 1
     args.state.menu_option_outline = [x: 180, y: 416, w: 256, h: 64, path: 'sprites/menu-option-outline.png']
     if args.inputs.keyboard.enter or args.inputs.mouse.click
       args.state.next_scene = :game_scene
       args.audio[:starting_bell] = {input: "sounds/blastwave_fx_boxingbellring_s08sp.136.mp3", gain: 0.2}
     end
   end
-  if args.state.menu_option == 2
+  if args.state.menu_option_main == 2
     args.state.menu_option_outline = [x: 180, y: 316, w: 256, h: 64, path: 'sprites/menu-option-outline.png']
     if args.inputs.keyboard.enter or args.inputs.mouse.click
       args.state.next_scene = :how_to_play_scene
     end
   end
-  if args.state.menu_option == 3
+  if args.state.menu_option_main == 3
     args.state.menu_option_outline = [x: 180, y: 216, w: 256, h: 64, path: 'sprites/menu-option-outline.png']
+    if args.inputs.keyboard.enter or args.inputs.mouse.click and args.state.menu_option_click_cooldown <= 0
+      args.state.next_scene = :options_scene
+      args.state.menu_option_click_cooldown += 10
+    end
+  end
+  if args.state.menu_option_main == 4
+    args.state.menu_option_outline = [x: 180, y: 116, w: 256, h: 64, path: 'sprites/menu-option-outline.png']
     if args.inputs.keyboard.enter or args.inputs.mouse.click
       GTK.request_quit
     end
@@ -119,6 +138,68 @@ def tick_how_to_play_scene args
       args.state.next_scene = :menu_scene
     end
   end
+end
+
+def tick_options_scene args
+  args.state.cpu_attack_warning_button ||= {x: 180, y: 546, w: 384, h: 64}
+  args.state.back_to_menu_button ||= {x: 180, y: 96, w: 384, h: 64}
+  args.state.button_outline ||= [x: 1300, y: 800, w: 384, h: 64, path: 'sprites/menu-option-outline.png']
+  args.state.menu_option ||= 1
+  args.state.menu_option_cooldown ||= 0
+
+  if args.inputs.up and args.state.menu_option_cooldown <= 0 and args.state.menu_option >= 2
+    args.state.menu_option_cooldown += 10
+    args.state.menu_option -= 1
+  end
+  if args.inputs.down and args.state.menu_option_cooldown <= 0 and args.state.menu_option <= 1
+    args.state.menu_option_cooldown += 10
+    args.state.menu_option += 1
+  end
+
+  if args.state.menu_option_cooldown > 0
+    args.state.menu_option_cooldown -= 1
+  end
+
+  if args.state.menu_option_click_cooldown > 0
+    args.state.menu_option_click_cooldown -= 1
+  end
+
+  if args.inputs.mouse.intersect_rect?(args.state.cpu_attack_warning_button)
+    args.state.menu_option = 1
+  end
+  if args.inputs.mouse.intersect_rect?(args.state.back_to_menu_button)
+    args.state.menu_option = 2
+  end
+
+  if args.state.menu_option == 1
+    args.state.button_outline = [x: 160, y: 546, w: 390, h: 64, path: 'sprites/menu-option-outline.png']
+    if args.inputs.keyboard.enter or args.inputs.mouse.click and args.state.menu_option_click_cooldown <= 0
+      if args.state.cpu_attack_warning == true
+        args.state.cpu_attack_warning = false
+      elsif args.state.cpu_attack_warning == false
+        args.state.cpu_attack_warning = true
+      end
+      args.state.menu_option_click_cooldown += 10
+    end
+  end
+  if args.state.menu_option == 2
+    args.state.button_outline = [x: 160, y: 96, w: 220, h: 64, path: 'sprites/menu-option-outline.png']
+    if args.inputs.keyboard.enter or args.inputs.mouse.click and args.state.menu_option_click_cooldown <= 0
+      args.state.next_scene = :menu_scene
+      args.state.menu_option_click_cooldown += 10
+    end
+  end
+
+  args.outputs.background_color = [255, 255, 255]
+  args.outputs.labels << {x: 180, y: 600, text: "CPU Attack Warning: ", size_enum: 10, a: 255, r: 0, g: 0, b: 0}
+  if args.state.cpu_attack_warning == true
+    args.outputs.labels << {x: 600, y: 600, text: "Enabled", size_enum: 10, a: 255, r: 0, g: 0, b: 0}
+  end
+  if args.state.cpu_attack_warning == false
+    args.outputs.labels << {x: 600, y: 600, text: "Disabled", size_enum: 10, a: 255, r: 0, g: 0, b: 0}
+  end
+  args.outputs.labels << {x: 180, y: 150, text: "Main Menu.", size_enum: 10, a: 255, r: 0, g: 0, b: 0}
+  args.outputs.primitives << args.state.button_outline
 end
 
 def tick_game_scene args
@@ -186,7 +267,9 @@ def tick_game_scene args
   collision_detection args
 
   args.outputs.background_color = [255, 255, 255]
-  args.outputs.primitives << args.state.cpu_one_attack_zone
+  if args.state.cpu_attack_warning == true
+    args.outputs.primitives << args.state.cpu_one_attack_zone
+  end
   args.outputs.primitives << args.state.player_one
   args.outputs.primitives << args.state.player_one_health_bar
   args.outputs.primitives << args.state.cpu_one
@@ -216,16 +299,16 @@ def tick_game_over_scene args
   args.state.back_to_menu_button ||= {x: 500, y: 96, w: 384, h: 64}
   args.state.play_again_button ||= {x: 180, y: 96, w: 384, h: 64}
   args.state.back_to_menu_button_outline ||= [x: 1300, y: 800, w: 384, h: 64, path: 'sprites/menu-option-outline.png']
-  args.state.menu_option ||= 1
+  args.state.menu_option_game_over ||= 1
   args.state.menu_option_cooldown ||= 0
 
-  if args.inputs.left and args.state.menu_option_cooldown <= 0 and args.state.menu_option >= 2
+  if args.inputs.left and args.state.menu_option_cooldown <= 0 and args.state.menu_option_game_over >= 2
     args.state.menu_option_cooldown += 10
-    args.state.menu_option -= 1
+    args.state.menu_option_game_over -= 1
   end
-  if args.inputs.right and args.state.menu_option_cooldown <= 0 and args.state.menu_option <= 2
+  if args.inputs.right and args.state.menu_option_cooldown <= 0 and args.state.menu_option_game_over <= 2
     args.state.menu_option_cooldown += 10
-    args.state.menu_option += 1
+    args.state.menu_option_game_over += 1
   end
 
   if args.state.menu_option_cooldown > 0
@@ -233,10 +316,10 @@ def tick_game_over_scene args
   end
 
   if args.inputs.mouse.intersect_rect?(args.state.play_again_button)
-    args.state.menu_option = 1
+    args.state.menu_option_game_over = 1
   end
   if args.inputs.mouse.intersect_rect?(args.state.back_to_menu_button)
-    args.state.menu_option = 2
+    args.state.menu_option_game_over = 2
   end
 
   args.outputs.background_color = [255, 255, 255]
@@ -292,7 +375,7 @@ def tick_game_over_scene args
 
   args.outputs.primitives << args.state.back_to_menu_button_outline
 
-  if args.state.menu_option == 1
+  if args.state.menu_option_game_over == 1
     args.state.back_to_menu_button_outline = [x: 160, y: 96, w: 256, h: 64, path: 'sprites/menu-option-outline.png']
     if args.inputs.keyboard.enter or args.inputs.mouse.click
       args.state.next_scene = :game_scene
@@ -300,7 +383,7 @@ def tick_game_over_scene args
       reset_defaults args
     end
   end
-  if args.state.menu_option == 2
+  if args.state.menu_option_game_over == 2
     args.state.back_to_menu_button_outline = [x: 480, y: 96, w: 256, h: 64, path: 'sprites/menu-option-outline.png']
     if args.inputs.keyboard.enter or args.inputs.mouse.click
       args.state.next_scene = :menu_scene
@@ -335,4 +418,5 @@ def debug args
   args.outputs.labels << [x: 10, y: 170, text: "player_one fist dy: #{(args.state.player_one_fist_right[:dx])}", size_enum: 3, a: 255, r: 0, g: 0, b: 0]
   args.outputs.labels << [x: 10, y: 200, text: "cpu one fist_right dx: #{(args.state.cpu_one_fist_right[:dx])}", size_enum: 3, a: 255, r: 0, g: 0, b: 0]
   args.outputs.labels << [x: 10, y: 230, text: "cpu one fist_right timer: #{(args.state.cpu_one_fist_right_timer_started)}", size_enum: 3, a: 255, r: 0, g: 0, b: 0]
+  args.outputs.labels << [x: 10, y: 260, text: "cpu_attack_warning: #{(args.state.cpu_attack_warning)}", size_enum: 3, a: 255, r: 0, g: 0, b: 0]
 end
